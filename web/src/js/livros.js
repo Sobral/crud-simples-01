03beba61f021578f198b254658dbb4933bcab434
Luciano Sobral <sobral.luciano@gmail.com>
2019-01-02 20:04:49 -0200

    Funcao para remover livro foi adicionada

--- web/src/js/livros.js
+++ web/src/js/livros.js
@@ -1,9 +1,4 @@
 let livros = [
-  {
-    "nome": "fanta",
-    "autor" : "coca",
-    "isbn": 3425
-  }
 ]
 
 var AdicionarLivro = function(e){
@@ -12,7 +7,6 @@ var AdicionarLivro = function(e){
   nome = document.getElementById("nome_livro").value;
   autor = document.getElementById("nome_autor").value;
   isbn = document.getElementById("isbn_code").value;
-
   livros.push({
     nome: nome,
     autor: autor,
@@ -26,15 +20,25 @@ var MostrarLivros = function () {
   let output = ""
   livros.forEach(function(livro){
     output +=
-    `<tr>
+    `<tr id='${livro.isbn}'>
      <td>${livro.nome}</td>
      <td>${livro.autor}</td>
      <td>${livro.isbn}</td>
+     <td><button onclick="ExcluirLivro(${livro.isbn},MostrarLivros)">Excluir</button></td>
      </tr>
     `
   });
 
   document.getElementById("conteudo").innerHTML = output;
+};
+
+var ExcluirLivro = function (elem, callback){
+
+  livros = livros.filter(function(livro) {
+    return (livro.isbn !== elem.id);
+  });
+
+  callback();
 };
 
 window.addEventListener('load', MostrarLivros);
