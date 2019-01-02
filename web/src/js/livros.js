6c593259f89d37cc8b3194f4d3f64a94660a4696
Luciano Sobral <sobral.luciano@gmail.com>
2019-01-01 22:11:56 -0200

    Funcao para adicionar um registro de livro atualizado, também foram feitas mudanças na estrutura do index.html para acompanhar as melhorias

--- web/src/js/livros.js
+++ web/src/js/livros.js
@@ -1,16 +1,19 @@
 let livros = []
 
-function AdicionarLivro(){
+function AdicionarLivro(e){
+  e.preventDefault()
 
-  nome = document.getElementById("nome_livro");
-  autor = document.getElementById("nome_autor");
-  isbn = document.getElementById("isbn_code");
+  nome = document.getElementById("nome_livro").value;
+  autor = document.getElementById("nome_autor").value;
+  isbn = document.getElementById("isbn_code").value;
+
   livros.push({
     nome: nome,
     autor: autor,
     isbn: isbn
   });
 
+  document.getElementById("formulario").reset();
 };
 
 document.getElementById('botao-enviar').addEventListener('click', AdicionarLivro);
